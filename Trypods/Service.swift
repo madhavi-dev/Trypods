ca48084ccec086193734ab63db7289107ffca661
Madhavi Yamani <>
2020-03-25 16:05:31 -0700

    testing again

---
+++ Trypods/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  Trypods
+//
+//  Created by Madhavi Yamani on 3/25/20.
+//  Copyright © 2020 info. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    private init() {
+        
+    }
+    public static func doSomething() -> String {
+        return "testing"
+    }
+}
---
+++ Trypods/Trypods/Trypods.h
@@ -0,0 +1,19 @@
+//
+//  Trypods.h
+//  Trypods
+//
+//  Created by Madhavi Yamani on 3/25/20.
+//  Copyright © 2020 info. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Trypods.
+FOUNDATION_EXPORT double TrypodsVersionNumber;
+
+//! Project version string for Trypods.
+FOUNDATION_EXPORT const unsigned char TrypodsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Trypods/PublicHeader.h>
+
+
